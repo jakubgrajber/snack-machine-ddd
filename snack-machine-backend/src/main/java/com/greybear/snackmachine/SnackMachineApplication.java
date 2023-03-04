884f00985de27d250be6ded461fa638308b27554
Jakub Grajber <grajber.jakub@gmail.com>
2023-03-04 11:11:36 +0100

    Add Spring Boot dependencies
    Thymeleaf as a template engine

---
+++ snack-machine-backend/src/main/java/com/greybear/snackmachine/SnackMachineApplication.java
@@ -0,0 +1,11 @@
+package com.greybear.snackmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackMachineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnackMachineApplication.class, args);
+    }
+}
---
+++ snack-machine-backend/src/test/java/com/greybear/snackmachine/SnackMachineApplicationTest.java
@@ -0,0 +1,11 @@
+package com.greybear.snackmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnackMachineApplicationTest {
+
+    @Test
+    void contextLoads() {}
+}
