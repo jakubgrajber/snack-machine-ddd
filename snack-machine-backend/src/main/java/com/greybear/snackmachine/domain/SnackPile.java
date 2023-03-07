f01599a83ea56d0cada6c8dd19f61bbbcf3e2ebd
Jakub Grajber <grajber.jakub@gmail.com>
2023-03-07 06:07:37 +0100

    Fix NPE

--- snack-machine-backend/src/main/java/com/greybear/snackmachine/domain/SnackPile.java
+++ snack-machine-backend/src/main/java/com/greybear/snackmachine/domain/SnackPile.java
@@ -7,9 +7,11 @@ import java.math.BigDecimal;
 public record SnackPile(Snack snack, int quantity, BigDecimal price) {
 
     public SnackPile {
-        if (quantity < 0) throw new IllegalArgumentException("The quantity cannot be less than zero.");
-        if (price.compareTo(BigDecimal.ZERO) < 0) throw new IllegalArgumentException("The price cannot be less than zero.");
-        if (price.remainder(new BigDecimal("0.01")).compareTo(BigDecimal.ZERO) > 0)
+        if (quantity < 0)
+            throw new IllegalArgumentException("The quantity cannot be less than zero.");
+        if (price != null && price.compareTo(BigDecimal.ZERO) < 0)
+            throw new IllegalArgumentException("The price cannot be less than zero.");
+        if (price != null && price.remainder(new BigDecimal("0.01")).compareTo(BigDecimal.ZERO) > 0)
             throw new IllegalArgumentException("The value of the price cannot be more precise than one cent.");
     }
 
